1ec1f5545b457abc201900d6c8117d7f6f3f5e4c
Jason Bartz <jrbartz@gmail.com>
2011-12-20 16:48:57 -0500

    added setup.py so it can be pip installed

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='autocomplete',
+        version='0.1',
+        description='The Washington Post\'s app for creating admin foreign key autocompletion fields.',
+        author='Jeremy Bowers',
+        author_email='jeremyjbowers@gmail.com',
+        url='https://github.com/jeremyjbowers/django-autocomplete',
+        packages = ['autocomplete',],
+        license = 'MIT',
+        package_data = {'': [
+            'media/autocomplete/js/*.js',
+            'media/autocomplete/css/*.css',
+            'templates/admin/autocomplete/*.html',
+        ]},
+        classifiers=[
+            'Environment :: Web Environment',
+            'Framework :: Django',
+            'Intended Audience :: Developers',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Utilities'
+        ],
+)
